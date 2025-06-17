78be2f2d475f22e55ef60838e23315e1c378c8c8
Jacky <dy2001mail@126.com>
2025-06-17 12:05:16 +0800

    Update for the sample

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -1,8 +1,9 @@
+
 package com.example.calculatorapp;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 import com.example.calculatorlib.Calculator;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView textView = new TextView(this);
-        int result1 = Calculator.add(5, 3);
-        int result2 = Calculator.subtract(10, 4);
-        textView.setText("5 + 3 = " + result1 + "\n10 - 4 = " + result2);
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3, 4);
+        textView.setText("3 + 4 = " + result);
         setContentView(textView);
     }
 }
--- CalculatorApp/calculatorlib/src/main/java/com/example/calculatorlib/Calculator.java
+++ CalculatorApp/calculatorlib/src/main/java/com/example/calculatorlib/Calculator.java
@@ -1,11 +1,12 @@
+
 package com.example.calculatorlib;
 
 public class Calculator {
-    public static int add(int a, int b) {
+    public int add(int a, int b) {
         return a + b;
     }
 
-    public static int subtract(int a, int b) {
+    public int subtract(int a, int b) {
         return a - b;
     }
 }
